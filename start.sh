84f736e6acd06b98e2cc608a78f4b50cafac1512
GroundNF <96753557+GroundNF@users.noreply.github.com>
2022-01-21 14:32:43 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ then
     vncpassword="insecure"
 fi
 
-printf "$vncpassword\n$vncpassword\n\n" | sudo vncserver :1
+printf "$vncpassword\n$vncpassword\n\n" | vncserver :1
 /.novnc/utils/launch.sh --vnc localhost:5901 --listen 8080
